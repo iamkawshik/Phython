4df7942d01b7793d3346a503d600644765043e1c
Kawshik Ahmed Ornob <97808635+iamkawshik@users.noreply.github.com>
2023-01-31 12:17:55 +0600

    no commit

---
+++ calculate the number of days between two dates.py
@@ -0,0 +1,10 @@
+from datetime import date
+f = date(2014, 7, 2)
+l = date(2014, 7, 11)
+d = l-f
+print(str(d.days) +" days")
+
+f = str(date(input()))
+l = ste(date(input()))
+d = l- f
+print(str(d.days) + " days")
---
+++ calculate the sum of three given numbers. If the values are equal, return three times their sum.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+c = int(input())
+sum = a + b + c
+if a == b == c:
+    sum = (a + b + c) * 3
+print(sum)
